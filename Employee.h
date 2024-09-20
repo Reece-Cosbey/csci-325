43dbd9262a8d6b46879392dc8cae80ce0b1dbe74
Reece-Cosbey <122653788+Reece-Cosbey@users.noreply.github.com>
2024-09-20 12:30:58 -0500

    Add files via upload

---
+++ Employee.h
@@ -0,0 +1,25 @@
+/* @file Employee.h
+ * @author Dr. Kericson
+ * @date 2023-9-22
+ */
+
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
+
+class Employee {
+ private:
+  int ID; //employee ID
+  int years;
+  
+ protected:
+  double hourlyRate;
+  float hoursWorked;
+ public:
+  virtual void print();
+  virtual double calculatePay();
+  void anniversary();
+  Employee();
+  Employee(int ID, int years, double hourlyRate, float hoursWorked);
+};
+
+#endif //EMPLOYEE_H
